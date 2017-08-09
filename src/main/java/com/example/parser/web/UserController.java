9df9117387c6bbe476b05a1f03e52e7816bbe49b
YuriyLoboda <loboda25yuriy@Gmail.com>
2017-08-09 14:45:45 +0300

    multipart file

--- src/main/java/com/example/parser/web/UserController.java
+++ src/main/java/com/example/parser/web/UserController.java
@@ -3,7 +3,9 @@ package com.example.parser.web;
 import com.example.parser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by yulo0717 on 8/7/2017.
@@ -14,7 +16,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/add")
-    public void setUser(){
+    public void setUser(@RequestBody MultipartFile file){
         userService.saveUserFromFile();
     }
 }
